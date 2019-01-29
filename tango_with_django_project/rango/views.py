b89bce06002746df6057b0c11727121c05d35bd9
Jack Watters <2314993W@student.gla.ac.uk>
2019-01-29 15:05:20 +0000

    Chapter 3

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>Index</a>")
+
+# Create your views here.
